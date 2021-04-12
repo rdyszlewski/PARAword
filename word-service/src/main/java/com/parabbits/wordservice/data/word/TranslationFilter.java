1ee350bdbd62efad0da7d1ce56d5c3380be1d8b3
rdyszlewski <parabitsdev@gmail.com>
2021-04-12 19:38:32 +0200

    Fix merge

--- word-service/src/main/java/com/parabbits/wordservice/data/word/TranslationFilter.java
+++ word-service/src/main/java/com/parabbits/wordservice/data/word/TranslationFilter.java
@@ -7,14 +7,17 @@ import lombok.Getter;
 @Builder(builderMethodName = "innerBuilder")
 public class TranslationFilter {
 
+    @Builder.Default
     private final long userId = -1;
     private final String name;
     private final String word;
     private final String description;
+    @Builder.Default
     private final long collectionId = -1;
     private final PartOfSpeech partOfSpeech;
     @Builder.Default
     private final int meaning = -1;
+    @Builder.Default
     private final long languageId = -1;
 
     public static TranslationFilter.TranslationFilterBuilder builder(long userId) {
